ef4102ac849a00463fa2d9666e7ef5ff54726143
loic <loic.gstrn@gmail.com>
2024-01-09 08:49:49 +0100

    demo hibernate

---
+++ demo_hibernate/src/main/java/heritage/Main.java
@@ -0,0 +1,79 @@
+package heritage;
+
+import heritage.single_table.CreditCardPayment1;
+import heritage.single_table.PayPalPayment1;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+        SessionFactory sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+
+        Session session = sessionFactory.openSession();
+        Transaction tx = null;
+        try {
+            tx = session.getTransaction();
+            tx.begin();
+            // Exemple joined table :
+
+    /*      CreditCardPayment creditCardPayment = new CreditCardPayment();
+            creditCardPayment.setCardNumber("123456");
+            creditCardPayment.setAmount(5000.0);
+            creditCardPayment.setPaymentDate(new Date());
+            creditCardPayment.setExpirationDate("12/2025");
+
+
+            PayPalPayment payPalPayment = new PayPalPayment();
+            payPalPayment.setAccountNumber("45879");
+            payPalPayment.setPaymentDate(new Date());
+            payPalPayment.setAmount(9854.5);
+
+            session.save(creditCardPayment);
+            session.save(payPalPayment);
+
+
+            System.out.println("creditCardPayment " + creditCardPayment);
+            System.out.println("payPalPayment " + payPalPayment);*/
+
+
+            // Exemple signe table :
+            CreditCardPayment1 creditCardPayment1 = new CreditCardPayment1();
+            creditCardPayment1.setCardNumber("545487");
+            creditCardPayment1.setPaymentDate(new Date());
+            creditCardPayment1.setExpirationDate("02/2056");
+            creditCardPayment1.setAmount(98214.25);
+
+            PayPalPayment1 payPalPayment1 = new PayPalPayment1();
+            payPalPayment1.setAccountNumber("8764640");
+            payPalPayment1.setAmount(874597.254);
+            payPalPayment1.setPaymentDate(new Date());
+
+            session.save(payPalPayment1);
+            session.save(creditCardPayment1);
+
+            System.out.println("creditCardPayment " + creditCardPayment1);
+            System.out.println("payPalPayment " + payPalPayment1);
+
+            tx.commit();
+
+        } catch (Exception ex) {
+            if (tx != null) {
+                tx.rollback();
+                ex.printStackTrace();
+            }
+        } finally {
+            session.close();
+            sessionFactory.close();
+        }
+
+
+    }
+}
---
+++ demo_hibernate/src/main/java/heritage/single_table/CreditCardPayment1.java
@@ -0,0 +1,39 @@
+package heritage.single_table;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("2")
+public class CreditCardPayment1 extends Payment1 {
+
+    private String cardNumber;
+
+    private String expirationDate;
+
+
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public String getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(String expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    @Override
+    public String toString() {
+        return "CreditCardPayment{" +
+                "cardNumber='" + cardNumber + '\'' +
+                ", expirationDate='" + expirationDate + '\'' +
+                "} " + super.toString();
+    }
+}
